ddd0cbb53458dc412b5a94dfbbe65026bcd0b862
BinaryKenny <khairullinvadim2006@gmail.com>
2026-05-15 17:46:21 +0300

    I added DataStruct

--- khairullin.vadim/T2/main.cpp
+++ khairullin.vadim/T2/main.cpp
@@ -5,6 +5,24 @@
 #include <iterator>
 #include <vector>
 #include <iomanip>
+#include <complex>
+
+struct IntLL
+{
+  long long int u;
+};
+
+struct Complex
+{
+  std::complex< double > c;
+};
+
+struct DataStruct
+{
+  IntLL key1;
+  Complex key2;
+  std::string key3;
+};
 
 int main()
 {}
