742964d536af91ab49aa097a32a237416e73ad4e
BinaryKenny <khairullinvadim2006@gmail.com>
2026-05-30 14:00:24 +0300

    I delete all superflows

--- khairullin.vadim/T3/main.cpp
+++ khairullin.vadim/T3/main.cpp
@@ -6,17 +6,10 @@
 #include <iterator>
 #include "Command.h"
 #include "Line.h"
-#include "Line.cpp"
-#include "Polygon.cpp"
-#include "Command.cpp"
-#include "Functions.cpp"
-#include "Point.cpp"
-#include "PointCompare.cpp"
 using ist_t = std::istream_iterator< khairullin::Polygon >;
-using ost_t = std::ostream_iterator< void >;
 using out_t = std::ostream_iterator< std::string >;
 using lineIter = std::istream_iterator< khairullin::Line >;
-
+using namespace std::placeholders;
 int main(int argc, char ** argv)
 {
   if (argc != 2) {
@@ -26,8 +19,6 @@ int main(int argc, char ** argv)
   khairullin::Command program;
   std::ifstream input(argv[1]);
   std::copy_if(ist_t{input}, ist_t{}, std::back_inserter(program.polygons), khairullin::toAdd);
-  std::copy(program.polygons.begin(), program.polygons.end(),
-    std::ostream_iterator<khairullin::Polygon>(std::cout, "\n"));
   std::cout << std::fixed << std::setprecision(1);
   auto output = std::bind(khairullin::out, program, _1);
   std::transform(lineIter{std::cin}, lineIter{}, out_t{std::cout}, output);
